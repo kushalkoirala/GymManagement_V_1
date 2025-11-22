95c0bbf9d741df1be2dd255ef7e7a07be17263cc
Jyotishman Goswami <jyotishman2468@gmail.com>
2025-11-22 17:00:56 +0530

    google sso

--- drizzle/src/db/schema.ts
+++ drizzle/src/db/schema.ts
@@ -3,8 +3,8 @@ import {boolean, integer, pgTable, varchar} from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users",{
       id: integer().primaryKey().generatedAlwaysAsIdentity(),
       email: varchar({ length: 255 }).notNull().unique(),
-      first_name: varchar({ length: 255 }).notNull(),
-      last_name: varchar({ length: 255 }).notNull(),
-      phone_number: varchar({ length: 20 }).notNull(),  
+      first_name: varchar({ length: 255 }),
+      last_name: varchar({ length: 255 }),
+      phone_number: varchar({ length: 20 }),  
       is_active: boolean().notNull().default(true),
 })
