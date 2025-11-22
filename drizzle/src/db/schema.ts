3ffd1e4ab2a2a92e52a2e3eb18688690d4762691
Jyotishman Goswami <jyotishman2468@gmail.com>
2025-11-22 15:10:41 +0530

    login page ui

--- drizzle/src/db/schema.ts
+++ drizzle/src/db/schema.ts
@@ -1,8 +1,10 @@
-import {integer, pgTable, varchar} from "drizzle-orm/pg-core";
+import {boolean, integer, pgTable, varchar} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users",{
       id: integer().primaryKey().generatedAlwaysAsIdentity(),
-      name: varchar({ length: 255 }).notNull(),
-      age: integer().notNull(),
       email: varchar({ length: 255 }).notNull().unique(),
+      first_name: varchar({ length: 255 }).notNull(),
+      last_name: varchar({ length: 255 }).notNull(),
+      phone_number: varchar({ length: 20 }).notNull(),  
+      is_active: boolean().notNull().default(true),
 })
