2b7514ab9178fee63d68292b1a781d80d8dd24c4
Jyotishman Goswami <jyotishman2468@gmail.com>
2025-12-22 22:23:35 +0530

    updated

--- app/[subdomain]/page.tsx
+++ app/[subdomain]/page.tsx
@@ -1,45 +1,6 @@
-"use client";
-
-import { useState } from "react";
-
-export default function ClientsPage() {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-
-  async function addClient() {
-    const res = await fetch("/api/client", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        phone_number: phone,
-      }),
-    });
-
-    const data = await res.json();
-    console.log("Response:", data);
-
-    if (res.ok) alert("Client added!");
-    else alert("Error adding client");
-  }
-
-  return (
-    <div>
-      <input
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <input
-        placeholder="Phone"
-        value={phone}
-        onChange={(e) => setPhone(e.target.value)}
-      />
-
-      <button onClick={addClient}>Add Client</button>
-    </div>
-  );
+ 
+export default function Page() {
+   return (
+        <div>Subdomain</div>
+   )
 }
--- drizzle/src/db/schema.ts
+++ drizzle/src/db/schema.ts
@@ -4,38 +4,52 @@ import {
   pgTable,
   varchar,
   serial,
-  timestamp
+  timestamp,
+  pgEnum
 } from "drizzle-orm/pg-core";
 
-// Owner table
+/* User roles */
+export const userRoleEnum = pgEnum("user_role", [
+  "SUPER_ADMIN",
+  "GYM_OWNER",
+  "GYM_STAFF",
+]);
+
 export const usersTable = pgTable("users", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   email: varchar("email", { length: 255 }).notNull().unique(),
   first_name: varchar("first_name", { length: 255 }),
   last_name: varchar("last_name", { length: 255 }),
-  phone_number: varchar("phone_number", { length: 20 }).unique(),
-  is_active: boolean("is_active").notNull().default(false),
+  phone_number: varchar("phone_number", { length: 20 }),
+  role: userRoleEnum("role").notNull(),
+  is_active: boolean("is_active").notNull().default(true),
   created_at: timestamp("created_at").defaultNow().notNull(),
 });
 
-// Gym
-export const gymsTable = pgTable("gyms",{
-    id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar("name", { length: 255 }).notNull(),
-    slug: varchar("slug", { length: 255 }).notNull().unique(),
-    owner_id: integer("owner_id")
-    .references(() => usersTable.id)
-    .notNull(), // the creator of the gym (main admin)
-    is_active: boolean("is_active").notNull().default(true),
-    created_at: timestamp("created_at").defaultNow().notNull(),
-})
 
-// Gym clients table
+export const gymsTable = pgTable("gyms", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar("name", { length: 255 }).notNull(),
+  slug: varchar("slug", { length: 255 }).notNull().unique(),
+
+  owner_id: integer("owner_id")
+    .references(() => usersTable.id)
+    .notNull(),
+
+  is_active: boolean("is_active").notNull().default(true),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+});
+
 export const clientsTable = pgTable("clients", {
   id: serial("id").primaryKey(),
+
+  gym_id: integer("gym_id")
+    .notNull()
+    .references(() => gymsTable.id, { onDelete: "cascade" }),
+
   name: varchar("name", { length: 255 }).notNull(),
   phone_number: varchar("phone_number", { length: 20 }),
-  tenant_id: integer("tenant_id")
-    .notNull()
-    .references(() => usersTable.id),
+
+  is_active: boolean("is_active").notNull().default(true),
+  created_at: timestamp("created_at").defaultNow().notNull(),
 });
